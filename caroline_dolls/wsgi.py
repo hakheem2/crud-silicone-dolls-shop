ed7e337cda8abf8f4aeae967650384400ad711e6
Anyeng Mondeson <anyengmondesonmbaubeh@gmail.com>
2025-07-30 23:57:26 +0100

    creating a super user

--- caroline_dolls/wsgi.py
+++ caroline_dolls/wsgi.py
@@ -16,3 +16,21 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caroline_dolls.settings')
 application = get_wsgi_application()
 
 
+import django
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+
+django.setup()
+
+User = get_user_model()
+
+try:
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='hakheem',
+            email='hakheemwyatt2@gmail.com',
+            password='holli123.J'  # 👈 Change this to a secure password
+        )
+        print("Superuser created successfully!")
+except IntegrityError:
+    print("Superuser already exists.")

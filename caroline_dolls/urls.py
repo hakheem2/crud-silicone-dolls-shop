ddb89dabd94346762ba952b9fc431bf5375a5131
Anyeng Mondeson <anyengmondesonmbaubeh@gmail.com>
2025-08-01 09:58:07 +0100

    site map  fix

--- caroline_dolls/urls.py
+++ caroline_dolls/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('order/', include(('order.urls', 'order'), namespace='order')),
     path('', include('core.urls', namespace='core')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps, "protocol": "https"}, name="django.contrib.sitemaps.views.sitemap"),
     # global pages
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
